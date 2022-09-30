535e81403d76d52bbbcdc9ae9cfac0b136fec764
Glenn <glenn.reumers@appwise.be>
2022-09-30 11:25:32 +0200

    Create many to many relationship between categories and books

--- app/Models/Category.ts
+++ app/Models/Category.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, manyToMany, ManyToMany } from '@ioc:Adonis/Lucid/Orm'
+import Book from './Book'
 
 export default class Category extends BaseModel {
   @column({ isPrimary: true })
@@ -10,4 +11,7 @@ export default class Category extends BaseModel {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @manyToMany(() => Book)
+  public categories: ManyToMany<typeof Book>
 }
