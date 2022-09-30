cee49095675bdb0fbbf4dfe68091bfe089bfd0dc
Glenn <glenn.reumers@appwise.be>
2022-09-30 11:43:47 +0200

    Fix table namae

--- database/migrations/1664529165017_category_books.ts
+++ database/migrations/1664529165017_category_books.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'category_books'
+  protected tableName = 'book_category'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
